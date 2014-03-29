507f275e0de7b4eec44758793f26a543bce5d6bf
Phillip Marfoglio <dcphillip@gmail.com>
2014-03-29 15:00:23 -0500

    Finish phone tests

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -18,4 +18,9 @@ describe 'the person view', type: :feature do
     end
   end
 
+  it 'has links to edit phone numbers' do
+    person.phone_numbers.each do |phone|
+      expect(page).to have_link('edit', href: edit_phone_number_path(phone))
+    end
+  end
 end
