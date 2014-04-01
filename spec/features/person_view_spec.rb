5d5a74172c86ecea722a294585664285d3afb827
Phillip Marfoglio <dcphillip@gmail.com>
2014-04-01 02:07:07 -0500

    fixed email display

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -5,22 +5,39 @@ require 'capybara/rspec'
 describe 'the person view', type: :feature do
 
   let(:person) { Person.create(first_name: 'John', last_name: 'Doe') }
+  
+  describe 'phone numbers' do
+    before(:each) do
+      person.phone_numbers.create(number: "555-1234")
+      person.phone_numbers.create(number: "555-5678")
+      visit person_path(person)
+    end
+  
+  
+    it 'shows the phone numbers' do
+      person.phone_numbers.each do |phone|
+        expect(page).to have_content(phone.number)
+      end
+    end
 
-  before(:each) do
-    person.phone_numbers.create(number: "555-1234")
-    person.phone_numbers.create(number: "555-5678")
-    visit person_path(person)
-  end
-
-  it 'shows the phone numbers' do
-    person.phone_numbers.each do |phone|
-      expect(page).to have_content(phone.number)
+    it 'has links to edit phone numbers' do
+      person.phone_numbers.each do |phone|
+        expect(page).to have_link('edit', href: edit_phone_number_path(phone))
+      end
     end
   end
 
-  it 'has links to edit phone numbers' do
-    person.phone_numbers.each do |phone|
-      expect(page).to have_link('edit', href: edit_phone_number_path(phone))
+  describe 'email addresses' do
+    before(:each) do
+      person.email_addresses.create(address: "foo@example.com")
+      person.email_addresses.create(address: "bar@example.com")
+      visit person_path(person)
+    end
+  
+    it 'shows the email addresses' do
+      person.email_addresses.each do |email|
+        expect(page).to have_selector('li', text: email.address)
+      end
     end
   end
-end
+end  
